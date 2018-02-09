d18528f0a9ddb74acfd34f27d0dbf85bbed84fc0
Craig Sapp <craigsapp@gmail.com>
2018-02-09 09:46:56 -0800

    Added checkquality program.

---
+++ tools/checkquality.cpp
@@ -0,0 +1,40 @@
+//
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Thu Feb  8 22:17:15 PST 2018
+// Last Modified: Thu Feb  8 22:17:18 PST 2018
+// Filename:      checkquality.cpp
+// Web Address:   
+// Syntax:        C++
+// vim:           ts=3:nowrap:ft=text
+//
+// Description:   Report to standard error if there are any problems.
+//
+
+#include "RollImage.h"
+
+#include <vector>
+
+using namespace std;
+using namespace prp;
+
+///////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		cerr << "Usage: frameduplicates file.tiff\n";
+		exit(1);
+	}
+
+	RollImage roll;
+	if (!roll.open(argv[1])) {
+		cerr << "Input filename " << argv[1] << " cannot be opened" << endl;
+		exit(1);
+	}
+
+	roll.loadGreenChannel();
+	roll.analyze();
+	roll.printQualityReport();
+
+	return 0;
+}
+
