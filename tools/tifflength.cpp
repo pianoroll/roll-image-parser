7c980dfde4b99213c33000437f389938c7bb803f
Craig Sapp <craigsapp@gmail.com>
2018-02-17 14:41:55 -0800

    Print only the length of the tiff image (number of rows).

---
+++ tools/tifflength.cpp
@@ -0,0 +1,40 @@
+//
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Fri Feb 16 21:39:57 PST 2018
+// Last Modified: Fri Feb 16 21:40:00 PST 2018
+// Filename:      tifflength.cpp
+// Web Address:   
+// Syntax:        C++
+// vim:           ts=3:nowrap:ft=text
+//
+// Description:   Returns the number of rows in the TIFF image.  Workd on 32-bit TIFFs as
+//                well as 64-bit TIFFs (tiffinfo program only works on 32-bit TIFFs).
+//
+
+#include "TiffFile.h"
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace prp;
+
+///////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		cerr << "Usage: tifflength file.tiff\n";
+		exit(1);
+	}
+
+	TiffFile image;
+	if (!image.open(argv[1])) {
+		cerr << "Input filename " << argv[1] << " cannot be opened" << endl;
+		exit(1);
+	}
+
+	cout << image.getRows() << endl;
+
+	return 0;
+}
+
